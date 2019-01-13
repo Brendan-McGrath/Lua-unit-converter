d56d8629c7c0e9c0cc893bcbe6bcbd04d45f47af
Brendan McGrath <Brendan-McGrath@Outlook.com>
2019-01-13 14:07:15 -0500

    Changed licensing and fixed an incorrect energy conversion.

--- modules/Energy.lua
+++ modules/Energy.lua
@@ -1,12 +1,12 @@
 local energyTables = {
-	  j = { 1,		 true  }, -- Joule
-	 Ws = { 1,		 true  }, -- Watt - Second
-	 Wh = { 3600,	 true  }, -- Watt - Hour
-	boe = { 6.1239,  false }, -- Barrel Of Oil Equivalent
-	Btu = { 1054.5,  false }, -- British thermal unit
-	cal = { 4.184,	 false }, -- Calorie
-	Cal = { 4184,    false }, -- Kilo Calorie
-	 eV = { 1.6e-19, false }  -- Electron Volt
+	  j = { 1,	   true  }, -- Joule
+	 Ws = { 1,	   true  }, -- Watt - Second
+	 Wh = { 3600,	   true  }, -- Watt - Hour
+	boe = { 6.1239,    false }, -- Barrel Of Oil Equivalent
+	Btu = { 1055.0558, false }, -- British thermal unit
+	cal = { 4.184,	   false }, -- Calorie
+	Cal = { 4184,      false }, -- Kilo Calorie
+	 eV = { 1.6e-19,   false }  -- Electron Volt
 }
 
 local valueNametable = {
